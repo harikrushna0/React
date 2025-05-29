57a6e2c2f74189a025e21cf06768262f497a6ba6
harikrushna0 <111632888+harikrushna0@users.noreply.github.com>
2025-05-29 19:42:08 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About=()=> {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company dedicated to providing the best services.</p>
+    </div>
+  );
+}
+export default About;
